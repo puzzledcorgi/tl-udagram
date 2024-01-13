9f3346a6f484138db2442a6fc342a01df9527314
puzzledcorgi <corgi04.puzzled@icloud.com>
2024-01-13 11:19:15 +0000

    initial commit

---
+++ udagram/.ipynb_checkpoints/set_env-checkpoint.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=tludagramdbuser
+export POSTGRES_PASSWORD=as92mdRs2Wk321
+export POSTGRES_HOST=tl-udagram-db.ci07azshqmzc.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::tl-udagram-udacity-proj
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
